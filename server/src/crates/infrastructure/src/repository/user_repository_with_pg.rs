490bd6a6335ef04b1d1a4e00261fd7f3153f8ad1
sendo-kakeru <sendokakeru.js@gmail.com>
2025-03-26 00:08:10 +0900

    feat(test): user create test

--- server/src/crates/infrastructure/src/repository/user_repository_with_pg.rs
+++ server/src/crates/infrastructure/src/repository/user_repository_with_pg.rs
@@ -1,6 +1,7 @@
 use crate::model::user_model::UserModel;
 use domain::entity::user::User;
 use domain::interface::user_repository_interface::UserRepositoryInterface;
+// use main:
 
 #[derive(Debug, Clone)]
 pub struct UserRepositoryWithPg {
@@ -37,5 +38,37 @@ impl UserRepositoryInterface for UserRepositoryWithPg {
         })?;
 
         Ok(User::try_from(row)?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use domain::{
+        entity::user::User, interface::user_repository_interface::UserRepositoryInterface,
+    };
+
+    use super::UserRepositoryWithPg;
+
+    async fn connect() -> Result<sqlx::PgPool, sqlx::Error> {
+        dotenv::dotenv().ok();
+
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let pool = sqlx::postgres::PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&database_url)
+            .await?;
+
+        Ok(pool)
+    }
+
+    #[tokio::test]
+    async fn test() {
+        let pool = connect().await.expect("database should connect");
+        let user_repository = UserRepositoryWithPg::new(pool.clone());
+        let user = User::new("テストユーザー".into(), "test@example.com".into());
+        let created_user = user_repository.create(&user).await.unwrap();
+
+        assert_eq!(created_user.name, user.name);
+        assert_eq!(created_user.email, user.email);
     }
 }
